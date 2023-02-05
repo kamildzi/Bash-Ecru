3f369f86cb6d8563e2c9e8163800f31dfe9a351f
Kamil D <kamildzi@users.noreply.github.com>
2023-02-05 08:41:34 +0100

    Runner updated. (#5)

--- _Core/PreLoad/000-BaseVars.sh
+++ _Core/PreLoad/000-BaseVars.sh
@@ -2,7 +2,7 @@
 # Base variables, for common use
 
 # Framework Version
-declare -rg FRAMEWORK_VER='0.82'
+declare -rg FRAMEWORK_VER='0.83'
 
 # Project Root Path
 declare -rg SCRIPT_ROOT=$( readlink -f $( dirname "${BASH_SOURCE[0]}" )/../.. )
